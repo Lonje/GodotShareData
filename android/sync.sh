d6538c38c7a75348b6123585850f4cc951e887fc
Roman Vasilyev <roman@lonje.com>
2024-04-28 10:19:47 -0700

    sync

---
+++ android/sync.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mv sharing/build/outputs/aar/sharing-release.aar ../JumpJump4/addons/godotsharedata/bin/release/godotsharedata-0.0.1-release.aar
+mv sharing/build/outputs/aar/sharing-debug.aar   ../JumpJump4/addons/godotsharedata/bin/debug/godotsharedata-0.0.1-debug.aar
